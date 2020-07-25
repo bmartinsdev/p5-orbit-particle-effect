d8b30b56542c02c241c7d5deb551b539cf23ea6e
Bruno Rodrigues <brunorodrigues355@gmail.com>
2020-07-25 12:32:49 +0100

    fix: reduced cpu cost to increase performance

--- classes.js
+++ classes.js
@@ -1,42 +1,46 @@
 class Orbit {
-    pos;
-    vel;
-    acc;
-    speed;
-    history = [];
-    historySize = 160;
+  pos;
+  vel;
+  acc;
+  speed;
+  history = [];
+  lifespan = 160;
 
-    constructor(x, y, speed, history) {
-      this.pos = createVector(x, y);
-      this.vel = p5.Vector.random2D();
-      this.speed = speed;
-      this.historySize = history;
-      this.vel.mult(8);
-    }
-  
-    update(x, y) {
-      let follow = createVector(x, y);
-      this.acc = p5.Vector.sub(follow, this.pos);
-      this.acc.setMag(0.8);
-  
-      this.vel.add(this.acc);
-      this.vel.limit(this.speed);
-
-      this.pos.add(this.vel);
-      let v = createVector(this.pos.x, this.pos.y);
-      this.history.push(v);
-
-      if(this.history.length > this.historySize)
-        this.history.shift();
-    }
-
-    show() {
-      beginShape();
-      for(let i = 1; i < this.history.length; i++){
-        let opacity = i < 35 ? i: 35;
-        stroke(100, 100, 100, opacity);
-        line(this.history[i].x, this.history[i].y, this.history[i-1].x, this.history[i-1].y);
-      }
-      endShape();
-    };
+  constructor(x, y, speed, history) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.speed = speed;
+    this.lifespan = history;
+    this.vel.mult(8);
   }
+
+  update(x, y) {
+    let follow = createVector(x, y);
+    this.acc = p5.Vector.sub(follow, this.pos);
+    this.acc.setMag(0.8);
+
+    this.vel.add(this.acc);
+    this.vel.limit(this.speed);
+
+    this.pos.add(this.vel);
+    let v = createVector(this.pos.x, this.pos.y);
+    this.history.push(v);
+
+    if (this.history.length > this.lifespan) this.history.shift();
+  }
+
+  show() {
+    beginShape();
+    for (let i = 1; i < this.history.length; i++) {
+      let opacity = i < 35 ? i : 35;
+      stroke(50, 50, 50, opacity);
+      line(
+        this.history[i].x,
+        this.history[i].y,
+        this.history[i - 1].x,
+        this.history[i - 1].y
+      );
+    }
+    endShape();
+  }
+}
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-let slowSpeed = 0.4;
-let slowInc = 0.24;
-let rx = 14;
-let ry = 14;
-let orbitRadius = 140;
+p5.disableFriendlyErrors = false;
+const followInc = 0.24;
+const orbitInc = 0.02;
+const rx = 14;
+const ry = 14;
+const orbitRadius = 140;
 let orbitSpeed = 0.4;
-let orbitInc = 0.016;
+let followSpeed = 0.4;
 let wWidth = window.innerWidth;
 let wHeight = window.innerHeight;
 let orbitList = [];
@@ -13,21 +14,11 @@ let slowOrbitList = [];
 function setup() {
   createCanvas(wWidth, wHeight);
   frameRate(24);
-  for (var i = 0; i < 20; i++) {
-    orbitList[i] = new Orbit(
-      generateRandom(wWidth),
-      generateRandom(wHeight),
-      4,
-      160
-    );
+  for (let i = 0; i < 8; i++) {
+    orbitList[i] = new Orbit(wWidth / 2, wHeight / 2, 4, 120);
   }
-  for (var i = 0; i < 5; i++) {
-    slowOrbitList[i] = new Orbit(
-      generateRandom(wWidth),
-      generateRandom(wHeight),
-      3 + i / 6,
-      200
-    );
+  for (let i = 0; i < 2; i++) {
+    slowOrbitList[i] = new Orbit(wWidth / 2, wHeight / 2, 3 + i / 6, 50);
   }
 }
 
@@ -39,14 +30,21 @@ function draw() {
   let followX = wWidth / 2;
   let followY = wHeight / 2;
   clear();
-  followX = mouseX + rx * Math.cos(slowSpeed + 30);
-  followY = mouseY + ry * Math.sin(slowSpeed + 30);
+  if (
+    mouseX > 25 &&
+    mouseY > 25 &&
+    mouseX < wWidth - 25 &&
+    mouseY < wHeight - 25
+  ) {
+    followX = mouseX + rx * Math.cos(followSpeed + 30);
+    followY = mouseY + ry * Math.sin(followSpeed + 30);
+  }
   for (let i = 0; i < orbitList.length; i++) {
     orbitList[i].x = wWidth / 2 + orbitRadius * Math.cos(orbitSpeed + inc);
     orbitList[i].y = wHeight / 2 + orbitRadius * Math.sin(orbitSpeed + inc);
     inc = inc + 10;
   }
-  slowSpeed += slowInc;
+  followSpeed += followInc;
   orbitSpeed += orbitInc;
 
   for (let i = 0; i < orbitList.length; i++) {
@@ -62,7 +60,7 @@ function draw() {
   }
   stroke("#fff");
   fill("#fff");
-  ellipse(wWidth / 2, wHeight / 2, 139 * 2 - 1, 139 * 2 - 1);
+  ellipse(wWidth / 2, wHeight / 2, 138 * 2, 138 * 2);
 }
 
 function generateRandom(size) {
